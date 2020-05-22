8937a53758ddcce156d620bdd524cbd46fa59f6f
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-22 16:25:34 -0400

    started on admin page

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once('vendor/autoload.php');
+
+//Instantiate the F3 Base class
+$f3 = Base::instance();
+
+//run $f3
+$f3->route('GET / ', function(){
+    $view = new Template();
+    echo $view->render('views/admin.html');
+});
+
+$f3-> run();
