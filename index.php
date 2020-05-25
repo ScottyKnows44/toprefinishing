dd8faf99075eb4783346856c222d4d0ce366cc08
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-25 16:58:21 -0400

    index.php

--- index.php
+++ index.php
@@ -9,7 +9,23 @@ $f3 = Base::instance();
 //run $f3
 $f3->route('GET / ', function(){
     $view = new Template();
+    echo $view->render('views/home.php');
+});
+
+$f3->route('GET /admin ', function(){
+    $view = new Template();
     echo $view->render('views/admin.html');
 });
+
+$f3->route('GET /clients ', function(){
+    $view = new Template();
+    echo $view->render('views/clients.html');
+});
+
+$f3->route('GET /services', function(){
+    $view = new Template();
+    echo $view->render('views/services.html');
+});
+
 
 $f3-> run();
