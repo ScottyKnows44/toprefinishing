82ca1d0fed5e28024b81b1c932d399f4b0176e8f
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-29 16:29:26 -0400

    database connectivity connected

---
+++ controller/controller.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class controller
+{
+    private $_f3;
+    private $_database;
+
+    public function __construct($f3, $database)
+    {
+        $this->_f3 = $f3;
+        $this->_database = $database;
+    }
+
+    public function home()
+    {
+        $view = new Template();
+        echo $view->render('views/home.php');
+    }
+
+    public function admin()
+    {
+        $view = new Template();
+        echo $view->render('views/admin.html');
+    }
+
+    public function clients()
+    {
+        $view = new Template();
+        echo $view->render('views/clients.html');
+    }
+
+    public function services()
+    {
+        $view = new Template();
+        echo $view->render('views/services.html');
+    }
+
+
+}
--- index.php
+++ index.php
@@ -6,26 +6,24 @@ require_once("model/Database.php");
 //Instantiate the F3 Base class
 $f3 = Base::instance();
 
+$database = new Database();
+$controller = new controller($f3, $database);
+
 //run $f3
 $f3->route('GET / ', function(){
-    $view = new Template();
-    echo $view->render('views/home.php');
+    $GLOBALS['controller']->home();
 });
 
 $f3->route('GET /admin ', function(){
-    $view = new Template();
-    echo $view->render('views/admin.html');
+    $GLOBALS['controller']->admin();
 });
 
 $f3->route('GET /clients ', function(){
-    $view = new Template();
-    echo $view->render('views/clients.html');
+    $GLOBALS['controller']->clients();
 });
 
 $f3->route('GET /services', function(){
-    $view = new Template();
-    echo $view->render('views/services.html');
+    $GLOBALS['controller']->services();
 });
-
 
 $f3-> run();
