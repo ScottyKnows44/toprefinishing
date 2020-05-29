76a06146af5c2e5863da7a6310af69b8607080c1
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-29 16:10:40 -0400

    database started

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('vendor/autoload.php');
-
+require_once("model/Database.php");
 //Instantiate the F3 Base class
 $f3 = Base::instance();
 
---
+++ model/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Database
+{
+
+    function connect()
+    {
+        try{
+            if($_SERVER['USER'] == 'sthompso'){
+                require '/home/sthompso/config.php';
+            } else if($_SERVER['USER'] == 'tschloss'){
+                require '/home/tschloss/config.php';
+            } else if($_SERVER['USER'] == 'aatadina'){
+                require '/home/aatadina/config.php';
+            }
+            $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            echo 'Connected to Database!';
+            return $dbh;
+        } catch (PDOException $e){
+            echo $e->getMessage();
+            return;
+        }
+
+    }
+
+
+}
